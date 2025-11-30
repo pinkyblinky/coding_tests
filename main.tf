7c7a9c2377b87471cc6604d51180bbe32a2881f8
Alexander Sachs <pinkyblinky01@gmail.com>
2025-11-30 13:46:01 -0600

    added aks

--- main.tf
+++ main.tf
@@ -16,6 +16,31 @@ data "azurerm_resource_group" "ex000" {
   name = "ex000"
 }
 
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "ex000aks"
+  location            = data.azurerm_resource_group.ex000.location
+  resource_group_name = data.azurerm_resource_group.ex000.name
+  dns_prefix          = "ex000aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "example"
+  }
+}
+
 output "resource_group_name" {
   value = data.azurerm_resource_group.ex000.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.example.name
 }
