587f66e990a5bb89e69a866aca314214e8f9c64a
Alexander Sachs <pinkyblinky01@gmail.com>
2025-12-01 07:27:37 -0600

    script updates

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 . <(gpg --decrypt secrets.sh.gpg)
+touch ~/.kube/config
 echo "$KUBECONFIGCONTENT" > ~/.kube/config
+gpg -o terraform.tfstate --decrypt terraform.tfstate.gpg
+./install_terraform.sh
